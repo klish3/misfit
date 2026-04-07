a450b089b05f54524a4e6667b85312958e47dffc
Tawanda Kanyangarara <klish3@gmail.com>
2026-04-07 18:36:09 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ oxc: true })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
